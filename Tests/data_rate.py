43ce436bce2909f03cc49c27d1e2375189b66748
DavidPav123 <davidpavlicek123@gmail.com>
2024-03-18 01:24:11 -1000

    Adding code for data rate data collection

---
+++ Tests/data_rate.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+
+if __name__ == "__main__":
+    MESSAGES_PER_MINUTE: int = 5
+    WAIT_TIME: float = 60 / MESSAGES_PER_MINUTE
+
+    # Time to click on arduino IDE
+    time.sleep(5)
+
+    # Send the set number of messages with a wait inbettwen that adds up to 60 seconds
+    for _ in range(MESSAGES_PER_MINUTE):
+        pyautogui.press("up")
+        pyautogui.press("enter")
+        time.sleep(WAIT_TIME)
+
+    print("Finished")
